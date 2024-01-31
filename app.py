af4089bd15e0b8f09b8f8559282d2304d0a35df0
Laura Williams <lwilliams@keitercpa.com>
2024-01-31 15:07:14 -0500

    sidebar update

--- app.py
+++ app.py
@@ -9,133 +9,116 @@ st.set_page_config(
      page_title='Data Conversion Demo',
     #  layout="wide",
 )
-st.markdown("## Data Conversion Demo")
 
 
-# tab1, tab2, tab3 = st.tabs(["Data Conversion", "Data Visualization", "Summary Statistics"])
-tab1, tab2, tab3, tab4 = st.tabs(["Data Conversion", "Data Visualization", "Summary Statistics", "Anomalies"])
+def data_conversion():
+    st.markdown("## Data Conversion")
 
+    data_conv = data_clean.rename(columns={'EmployeeID':'Key','RegularHours':'E_Hourly Regular_Hours','OvertimeHours':'E_Overtime_Hours'})
+    st.dataframe(data_conv[['Key','E_Hourly Regular_Hours','E_Overtime_Hours']], hide_index=True, use_container_width=True)
 
-#%% TAB 1: Data Conversion
-with tab1:
-    col1,col2 = st.columns([.5,.5])
-    with col1:
-        data_conv = data_clean.rename(columns={'EmployeeID':'Key','RegularHours':'E_Hourly Regular_Hours','OvertimeHours':'E_Overtime_Hours'})
-        st.dataframe(data_conv[['Key','E_Hourly Regular_Hours','E_Overtime_Hours']], hide_index=True, use_container_width=True)
+def data_viz():    
+    st.markdown("## Data Visualization")
+    tab1, tab2 = st.tabs(['Plot 1','Plot 2'])
 
-    with col2:
-        st.empty()
+    with tab1:
+        data_viz = (
+            data_clean
+            .groupby(['EndDate'])
+            .agg({'TotalHours':'mean', 'OvertimeHours':'mean',})
+            .reset_index()
+            .rename(columns={'TotalHours':'Total Hours','OvertimeHours':'Overtime Hours'})
+        )
 
+        # Plot
+        plt1 = data_viz.plot(
+            kind='bar',
+            x='EndDate',
+            stacked=True,
+            xlabel='Pay Period End',
+            ylabel='Average Pay Period Hours',
+            rot=0,
+            figsize=(6.4,4.8)
+        )
+        # Move legend outside plot
+        plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
+        plt.title('With % OT Hours',fontsize=10)
+        plt.suptitle('Average Hours Worked by all Employees per Pay Period',x=.46,y=.95)
+        plt.tight_layout()
 
-#%% TAB 2: Data Visualization
-with tab2:
-    # col1, col2 = st.columns(2)
-
-    # with col1:
-    data_viz = (
-        data_clean
-        .groupby(['EndDate'])
-        .agg({'TotalHours':'mean', 'OvertimeHours':'mean',})
-        .reset_index()
-        .rename(columns={'TotalHours':'Total Hours','OvertimeHours':'Overtime Hours'})
-    )
-
-    # Plot
-    plt1 = data_viz.plot(
-        kind='bar',
-        x='EndDate',
-        stacked=True,
-        xlabel='Pay Period End',
-        ylabel='Average Pay Period Hours',
-        rot=0,
-        figsize=(6.4,4.8)
-    )
-    # Move legend outside plot
-    plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
-    plt.title('With % OT Hours',fontsize=10)
-    plt.suptitle('Average Hours Worked by all Employees per Pay Period',x=.46,y=.95)
-    plt.tight_layout()
-
-    # Add text labels
-    for x in plt1.containers:
-        # Only create labels on OT hours part
-        labels = [str(100*round(data_viz['Overtime Hours']/data_viz['Total Hours'],3)[i])+'%' if 0 < v.get_height() < 60 else '' for i,v in enumerate(x)]
-        # Add label to bar
-        plt1.bar_label(x, labels=labels, label_type='center')
+        # Add text labels
+        for x in plt1.containers:
+            # Only create labels on OT hours part
+            labels = [str(100*round(data_viz['Overtime Hours']/data_viz['Total Hours'],3)[i])+'%' if 0 < v.get_height() < 60 else '' for i,v in enumerate(x)]
+            # Add label to bar
+            plt1.bar_label(x, labels=labels, label_type='center')
+        
+        with st.container(border=True):
+            st.pyplot(plt1.figure)
     
-    # with st.container(border=True,height=500):
-    #     st.pyplot(plt1.figure)
+    with tab2:
+        anomalies = (
+            data_clean
+            .groupby(['Store','Description'])
+            .agg({'EmployeeName':'count'})
+            .reset_index()
+        )
 
-# with col2:
-    anomalies = (
-        data_clean
-        .groupby(['Store','Description'])
-        .agg({'EmployeeName':'count'})
-        .reset_index()
-        # .rename(columns={'EmployeeName'})
-    )
+        # Unique categories
+        categories = anomalies['Description'].unique()
+        stores = anomalies['Store'].unique()
+        colors = {'Store A': 'lightblue', 'Store B': 'green', 'Store C': 'purple', 'Store D': 'darkblue', 'Store E': 'pink', 'Store F': 'orange', 'Store G': 'indigo'}
 
-    # Unique categories
-    categories = anomalies['Description'].unique()
-    stores = anomalies['Store'].unique()
-    colors = {'Store A': 'lightblue', 'Store B': 'green', 'Store C': 'purple', 'Store D': 'darkblue', 'Store E': 'pink', 'Store F': 'orange', 'Store G': 'indigo'}
+        # Create a figure and axes
+        fig, axs = plt.subplots(nrows=2 ,ncols=2, sharex=True, sharey=True, figsize=(6.4,4.8))
+        axs = axs.flatten()
 
-    # Create a figure and axes
-    fig, axs = plt.subplots(nrows=2 ,ncols=2, sharex=True, sharey=True, figsize=(6.4,4.8))
-    axs = axs.flatten()
+        # Iterate over each category and create a plot
+        for ax, category in zip(axs, categories):
+            # Select Employee type and fill in 0s if a store does not have any of that employee type
+            sub_df = (anomalies[anomalies['Description'] == category]
+                    .set_index('Store').reindex(stores).fillna(value={'EmployeeName':0}).ffill().reset_index())
+            # Create sublplot
+            sub_df.plot(x='Store', y='EmployeeName', kind='bar', ax=ax, title=category, 
+                        color=[colors[x] for x in sub_df['Store']],ylim=(0,10),legend=False,xlabel='')
 
-    # Iterate over each category and create a plot
-    for ax, category in zip(axs, categories):
-        # Select Employee type and fill in 0s if a store does not have any of that employee type
-        sub_df = (anomalies[anomalies['Description'] == category]
-                .set_index('Store').reindex(stores).fillna(value={'EmployeeName':0}).ffill().reset_index())
-        # Create sublplot
-        sub_df.plot(x='Store', y='EmployeeName', kind='bar', ax=ax, title=category, 
-                    color=[colors[x] for x in sub_df['Store']],ylim=(0,10),legend=False,xlabel='')
-    
-    plt.suptitle('Number of Employees per Position per Store')
-    plt.tight_layout()
+        plt.suptitle('Number of Employees per Position per Store')
+        plt.tight_layout()
 
-    with st.container(border=True):
-        st.pyplot(plt1.figure)
-
-    with st.container(border=True):
-        st.pyplot(fig)
+        with st.container(border=True):
+            st.pyplot(fig)
 
 
-#%% TAB 3: Summary Statistics
-with tab3:
-    # col1,col2 = st.columns([1.5,2],gap='small')
-    
+def summary_stats():    
+    st.markdown("## Data Summary")
+
+    tab1, tab2 = st.tabs(['Summary Statistics','Categorical Frequencies'])
     data_stats = (
         data_clean
         .drop(columns=['EmployeeID','TimeCard','EmployeeName','EmployeeExtRef','ShortDescription','ExternalReference'])
     )
 
-    # with col1:
-    st.write('Summary Statistics')
-    st.dataframe(data_stats[data_stats.columns[[str(x)!='object' for x in data_stats.dtypes]]].describe().round(2))
+    with tab1:
+        # st.write('Summary Statistics')
+        st.dataframe(data_stats[data_stats.columns[[str(x)!='object' for x in data_stats.dtypes]]].describe().round(2))
 
-    # with col2:
-    st.write('Categorical Frequencies')
-    st.dataframe(data_stats[data_stats.columns[[str(x)=='object' for x in data_stats.dtypes]]].describe())
+    with tab2:
+        # st.write('Categorical Frequencies')
+        st.dataframe(data_stats[data_stats.columns[[str(x)=='object' for x in data_stats.dtypes]]].describe())
 
 
-#%% TAB 4: Anomalies
-with tab4:
-    # col1, col2 = st.columns([3,3])
+def anomalies():    
+    st.markdown("## Data Anomalies")
     data_stats2 = data_clean.drop(
         columns=['TimeCard','EmployeeName','EmployeeExtRef','ShortDescription','ExternalReference','RateTimeFrame']
         ).rename(columns={'OvertimeHours':'OTHours'})
-    
-    # with col1:
+
     st.write('Pay Periods Worked for Employees with Missing Break Time')
     st.dataframe((data_stats2[data_stats2.BreakTime.isna() & (data_stats2.RateType!='Salary')]
-                  [['EmployeeID','Store','Description','Minor','RateType',
+                    [['EmployeeID','Store','Description','Minor','RateType',
                     'BeginDate','EndDate','RegularHours','OTHours','TotalHours','BreakTime']]),
                     hide_index=True)
 
-    # with col2:
     if any(data_clean.groupby(['EmployeeID']).Store.count() < len(data_clean[['BeginDate','EndDate']].drop_duplicates())):
         missing_weeks_emps = (
             data_clean.groupby(['EmployeeID']).Store.count()
@@ -145,12 +128,20 @@ with tab4:
         
         st.write('Pay Periods Worked for Employees with Missing Pay Periods')
         st.dataframe((data_stats2[data_stats2['EmployeeID'].isin(missing_weeks_emps)]
-                      [['EmployeeID','Store','Description','Minor','RateType',
+                        [['EmployeeID','Store','Description','Minor','RateType',
                         'BeginDate','EndDate','RegularHours','OTHours','TotalHours']]),
                         hide_index=True)
-    else:
-        st.empty()
-        
+    else: st.empty()
 
-    
 
+#%% OUTPUT
+page_names_to_funcs = {
+    "Data Conversion": data_conversion,
+    "Data Visualization":data_viz,
+    "Data Summary":summary_stats,
+    "Data Anomalies":anomalies
+}
+
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
